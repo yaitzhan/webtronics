98c40a5ade80be8ede58575ea87b549e00510534
Yerzhan Atizhanov <e.aitzhanov@trafficwave.kz>
2023-07-10 00:58:28 +0600

    fix initial data script

--- webtronics/initial_data.py
+++ webtronics/initial_data.py
@@ -1,16 +1,21 @@
-import logging
-
-from webtronics.db.init_db import init_db
-from webtronics.db.session import SessionLocal
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+from webtronics import crud, schemas
+from webtronics.db.session import Session
+from webtronics.core.config import settings
+from webtronics.db import base  # noqa: F401
 
 
-def main() -> None:
-    db = SessionLocal()
-    init_db(db)
+async def init_db() -> None:
+    async with Session() as db:
+        user = await crud.user.get_by_email(db=db, email=settings.FIRST_SUPERUSER)
+        if not user:
+            user_in = schemas.UserCreate(
+                email=settings.FIRST_SUPERUSER,
+                password=settings.FIRST_SUPERUSER_PASSWORD,
+                is_superuser=True,
+            )
+            await crud.user.create(db, obj_in=user_in)
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    import asyncio
+    asyncio.run(init_db())
